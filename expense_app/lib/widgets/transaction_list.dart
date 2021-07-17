5e24d301e30747c13eb1c7d70644318d7b8f92b9
mxaba <samuel.mcebo@gmail.com>
2021-07-18 00:38:11 +0200

    added a hight

--- expense_app/lib/widgets/transaction_list.dart
+++ expense_app/lib/widgets/transaction_list.dart
@@ -10,7 +10,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      // height: 250,
+      height: 250,
       child: ListView(
         children: transaction.map((tx) {
           return Card(
